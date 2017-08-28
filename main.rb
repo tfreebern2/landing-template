3f1f07d7709b475c46ddb2b3e517cf02601f3cbe
Timothy Freebern II <tfreebern2@gmail.com>
2017-08-27 22:59:05 -0400

    Added Email Web Form. Installed Pony gem. Setup Pony send form code

--- main.rb
+++ main.rb
@@ -1,7 +1,43 @@
 require 'sinatra'
+require 'pony'
 
 set :port, 3000
 
 get '/' do
   erb :index
+end
+
+post '/' do
+  configure_pony
+  name = params[:name]
+  sender_email = params[:email]
+  message = params[:message]
+  logger.error params.inspect
+  begin
+    Pony.mail(
+      :from => "#{name}<#{sender_email}>",
+      :to => 'example@gmail.com',
+      :subject =>"#{name} has contacted you",
+      :body => "#{message}",
+    )
+    redirect '/success'
+  rescue
+    @exception = $!
+    erb :boom
+  end
+end
+
+def configure_pony
+  Pony.options = {
+    :via => :smtp,
+    :via_options => {
+      :address              => 'smtp.sendgrid.net',
+      :port                 => '587',
+      :user_name            => ENV['SENDGRID_USERNAME'],
+      :password             => ENV['SENDGRID_PASSWORD'],
+      :authentication       => :plain,
+      :enable_starttls_auto => true,
+      :domain               => 'heroku.com'
+    }
+  }
 end
